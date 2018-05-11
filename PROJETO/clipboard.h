cb930dfae6efba36b971c518554f8bd49685157f
InesGaspar <inesreisgaspar@gmail.com>
2018-05-11 17:57:21 +0100

    esmerdav1

--- PROJETO/clipboard.h
+++ PROJETO/clipboard.h
@@ -1,18 +1,3 @@
-#define INBOUND_FIFO "INBOUND_FIFO"
-#define OUTBOUND_FIFO "OUTBOUND_FIFO"
-#define REGIONS_NR 10
-#define COPY 0
-#define PASTE 1
-#define SOCK_ADDRESS "./CLIPBOARD_SOCKET"
-
-#include <sys/types.h>
-
-typedef struct Smessage{
-	int region;
-	int message_size;
-	int order;
-}Smessage;
-
 int clipboard_connect(char * clipboard_dir);
 int clipboard_copy(int clipboard_id, int region, void *buf, size_t count);
 int clipboard_paste(int clipboard_id, int region, void *buf, size_t count);
